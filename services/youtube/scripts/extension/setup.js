a855283b142421dfe7c494f6f2b62bb57780e372
thislooksfun <thislooksfun@repbot.org>
2018-03-17 16:27:25 -0500

    Clean up yt setup script

--- services/youtube/scripts/extension/setup.js
+++ services/youtube/scripts/extension/setup.js
@@ -1,78 +1,29 @@
 var vid = document.querySelector('#player .html5-main-video');
 
-window.wrappedJSObject.tlf_YTPlayer = cloneInto({status: 'unknown', shouldBePlaying: false}, window);
+var wjo = window.wrappedJSObject;
+
+wjo.tlf_YTPlayer = cloneInto({status: 'unknown'}, window);
+var ytp = wjo.tlf_YTPlayer;
 
 function selectorVisible(selector) {
   return getComputedStyle(document.querySelector(selector), null).display !== 'none';
 }
 
-var hasLoaded = false;
-
-// Mute when anything happens
-var mediaEvents = [
-  'abort',
-  'canplay',
-  'canplaythrough',
-  'durationchange',
-  'emptied',
-  'encrypted',
-  'ended',
-  'error',
-  'interruptbegin',
-  'interruptend',
-  'loadeddata',
-  'loadedmetadata',
-  'loadstart',
-  'mozaudioavailable',
-  'pause',
-  'play',
-  'playing',
-  'progress',
-  'ratechange',
-  'seeked',
-  'seeking',
-  'stalled',
-  'suspend',
-  'timeupdate',
-  'volumechange',
-  'waiting',
-];
-
-function eventHappened() {
-  var lg1 = 'b4: ' + vid.muted + ':' + vid.volume;
-  
-  var sbp = window.wrappedJSObject.tlf_YTPlayer.shouldBePlaying;
-  vid.muted = !sbp;
-  vid.volume = sbp ? 1.0 : 0.0;
-  
-  var lg2 = 'af: ' + vid.muted + ':' + vid.volume;
-  
-  console.log(lg1 + ' ' + lg2);
-}
-
-for (evt of mediaEvents) {
-  vid.addEventListener(evt, eventHappened, true);
-}
-
 setInterval(function() {
-  vid.muted = !window.wrappedJSObject.tlf_YTPlayer.shouldBePlaying;
-  
   var adShown = selectorVisible('#player .video-ads');
   var videoAd = adShown && selectorVisible('#player .video-ads .videoAdUi');
   
   if (adShown && videoAd) {
-    vid.muted = true;
-    if (window.wrappedJSObject.tlf_YTPlayer.status === 'main') {
-      window.wrappedJSObject.tlf_YTPlayer.status = 'postroll';
+    if (ytp.status === 'main') {
+      ytp.status = 'postroll';
     } else {
-      window.wrappedJSObject.tlf_YTPlayer.status = 'preroll';
-      window.wrappedJSObject.tlf_YTPlayer.canSkipAd = selectorVisible('#player .video-ads .videoAdUiSkipButton');
+      ytp.status = 'preroll';
+      ytp.canSkipAd = selectorVisible('#player .video-ads .videoAdUiSkipButton');
     }
   } else {
+    vid.muted = false;
     // Playing main video, possibly with a popup ad
-    window.wrappedJSObject.tlf_YTPlayer.status = 'main';
-    window.wrappedJSObject.tlf_YTPlayer.canSkipAd = undefined;
+    ytp.status = 'main';
+    ytp.canSkipAd = undefined;
   }
 }, 50);
-
-console.log('setup.js loaded');
--- services/youtube/scripts/play.js
+++ services/youtube/scripts/play.js
@@ -1,3 +1,2 @@
 var vid = document.querySelector('#player .html5-main-video');
-window.tlf_YTPlayer.shouldBePlaying = true;
 vid.play();
