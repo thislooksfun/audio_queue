9423f23fb44d54717a8f01272674054f3f2bac6d
thislooksfun <thislooksfun@repbot.org>
2018-03-20 18:35:02 -0500

    Add test placeholder

---
+++ test/player/player.test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+describe.skip("Player", function() {
+  it("should wait for something in the queue", function() {
+    
+  });
+  
+  it("should queue then play", function() {
+    
+  });
+  
+  it("should ???", function() {
+    
+  });
+});
---
+++ test/server/server.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+describe.skip("Server", function() {
+  it("should start", function() {
+    
+  });
+  
+  it("should advertise on Bonjour", function() {
+    
+  });
+});
