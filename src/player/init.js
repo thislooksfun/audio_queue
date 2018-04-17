0820b352b01674765be295042d0c3f0d00065c9e
thislooksfun <thislooksfun@repbot.org>
2018-04-17 00:07:40 -0500

    Add another debug message

--- src/player/init.js
+++ src/player/init.js
@@ -1,11 +1,14 @@
 "use strict";
 
+//
+
 // Local imports
 const services = pquire("helper/services");
 
 // The actual init function
 module.exports = async function() {
   log.trace("Initalizing...");
+  
+  log.debug("Loading services...");
   await services.load();
-  // await drivers.init();
 };
